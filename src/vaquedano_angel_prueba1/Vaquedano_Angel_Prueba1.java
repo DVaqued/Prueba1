a3c05710c1f12639fbdb8872ebd3159b1dd110ca
angel <angel@dvaqued>
2024-08-09 10:50:07 -0600

    Realizamos una pregunta de cuantos votos desea ingresar, que ingrese cada uno y analice si hay votos nulos, y luego haga el porcentaje. Despues que haga todos los calculos e imprima quien gano.

--- src/vaquedano_angel_prueba1/Vaquedano_Angel_Prueba1.java
+++ src/vaquedano_angel_prueba1/Vaquedano_Angel_Prueba1.java
@@ -18,7 +18,7 @@ public class Vaquedano_Angel_Prueba1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Scanner lea = new Scanner(System.in);
+        Scanner lea = new Scanner(System.in).useDelimiter("\n");
         Random random = new Random();
         int opciones = 0;
 
@@ -29,7 +29,12 @@ public class Vaquedano_Angel_Prueba1 {
         int numero = 0;
         int suma = 0;
 
-        //Ejercicio 3
+        //MENU
+        int cont1=0;
+        int cont2=0;
+        int cont3=0;
+        int cont4=0;
+        
         while (true) {
             System.out.println("\n---Menu---"
                     + "\n1. Palabra al reves."
@@ -48,9 +53,8 @@ public class Vaquedano_Angel_Prueba1 {
                         for (int contador = palabranormal.length() - 1; contador >= 0; contador--) {
                             System.out.print(palabranormal.charAt(contador)
                                     + "\n ");
+                            cont1++;
                         }
-
-                        break;
                     case 2:
                         System.out.println("Bienvenido al ejercicio numero perfecto!");
                         System.out.println("Favor ingrese el numero al que desea evaluar: ");
@@ -67,6 +71,7 @@ public class Vaquedano_Angel_Prueba1 {
                             System.out.println("Su numero no es perfecto.");
 
                         }
+                        cont2++;
 
                     case 3:
                         int numRandom = random.nextInt(100);
@@ -79,20 +84,85 @@ public class Vaquedano_Angel_Prueba1 {
                         }
                         if (suma2 == numRandom + 1) {
                             System.out.println("Su numero es primo.");
+                            System.out.println("Divisiores: 1 y " + numRandom);
                         } else {
                             System.out.println("No es un numero primo.");
                         }
-                    case 4:
+                        cont3++;
                         
+                    case 4:
+                        int votosporhacer = 0;
+                        int votoAzul = 0;
+                        int votoRojo = 0;
+                        int votoAmarillos = 0;
+                        int votoNegro = 0;
+                        int votoNulo = 0;
+                        int votostotal = 0;
+                        String voto = "";
 
-                        break;
+                        System.out.println("Cuantos votos desea ingresar: ");
+                        votosporhacer = lea.nextInt();
 
-                    case 5:
+                        for (int contador2 = 0; contador2 < votosporhacer; contador2++) {
+                                System.out.println("---PLANILLAS---"
+                                        + "\nAZUL"
+                                        + "\nROJO"
+                                        + "\nAMARILLO"
+                                        + "\nNEGRO");
+                                System.out.println("Favor ingrese que voto hara: ");
+                                voto = lea.next().toUpperCase();
+                            if (voto.equals("AZUL")) {
+                                votoAzul++;
+                                votostotal++;
+                            } else if (voto.equals("AMARILLO")) {
+                                votoAmarillos++;
+                                votostotal++;
+
+                            } else if (voto.equals("ROJO")) {
+                                votoRojo++;
+                                votostotal++;
+
+                            } else if (voto.equals("NEGRO")) {
+                                votoNegro++;
+                                votostotal++;
+
+                            } else {
+                                System.out.println("Voto nulo.");
+                                votoNulo++;
+                                votostotal-=votoNulo;
+                            }
+                        }
+                            double valido = (votostotal * 100) / votosporhacer;
+
+                            if (valido > 60) {
+                                if (votoAzul > votoRojo && votoAzul > votoNegro && votoAzul > votoAmarillos) {
+                                    System.out.println("Ganador partido AZUL con: " + votoAzul + " voto(s)");
+                                } else if (votoRojo > votoAzul && votoRojo > votoNegro && votoRojo > votoAmarillos) {
+                                    System.out.println("Ganador partido ROJO con: " + votoRojo + " voto(s)");
+                                } else if (votoNegro > votoRojo && votoNegro > votoAzul && votoNegro > votoAmarillos) {
+                                    System.out.println("Ganador partido NEGRO con: " + votoNegro + " voto(s)");
+                                } else if (votoAmarillos > votoAzul && votoAmarillos > votoRojo && votoAmarillos > votoNegro) {
+                                    System.out.println("Ganador partido AMARILLO con: " + votoAmarillos + " voto(s)");
+                                } else {
+                                    System.out.println("Gano nulos");
+                                }
+                            } else {
+                                System.out.println("No se realizo porque la cantidad de votos validos es el 60% de los votos totales");
+
+                                System.out.println("No gano nadie");
+                            }
+                            cont4++;
+
+                case 5:
+                        System.out.println("Se ha ingresado al ejercicio uno "+cont1+" veces."
+                                + "\nSe ha ingresado al ejercicio dos "+cont2+" veces."
+                                + "\nSe ha ingresado al ejercicio tres "+cont3+" veces."
+                                + "\nSe ha ingresado al ejercicio cuatro "+cont4+" veces.");
                         System.out.println("Gracias!");
                         System.exit(0);
-
                 }
-            } else {
+                
+                } else {
                 System.out.println("Favor ingrese una opcion valida");
             }
         }
